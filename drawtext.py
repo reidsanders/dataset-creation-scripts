3c1dbf255fbb5cabe83fad8fa286ff3a997f5a69
Reid Sanders <reid@reidsanders.net>
2022-03-04 17:55:28 -0500

    Black format

--- drawtext.py
+++ drawtext.py
@@ -15,14 +15,16 @@ import json
 from tqdm import tqdm
 from textwrap import wrap
 
+
 def str2bool(v):
     """Converts strings to appropriate bool, for argparse."""
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
 
 def get_args(arg_input):
     """Takes args input and returns them as a argparse parser
@@ -40,81 +42,58 @@ def get_args(arg_input):
         contains namespace with keys and values for each parser argument
 
     """
-    parser = argparse.ArgumentParser(
-        description='boilerplate arg parser'
-    )
+    parser = argparse.ArgumentParser(description="boilerplate arg parser")
+    parser.add_argument("--dirpath", type=str, default="input/", help="input dir")
+    parser.add_argument("--out", type=str, default="out/", help="out dir")
+    parser.add_argument("--split", type=str, default="sentence", help="sentence, word")
     parser.add_argument(
-        '--dirpath',
-        type=str,
-        default='input/',
-        help="input dir"
+        "--align", type=str, default="upperleft", help="left, upperleft, center"
     )
+    parser.add_argument("--font_size", type=int, default=24, help="font size")
     parser.add_argument(
-        '--out',
-        type=str,
-        default='out/',
-        help="out dir"
+        "--size",
+        type=lambda s: [int(item) for item in s.split(",")],
+        default="128,128",
+        help="size of output image",
     )
-    parser.add_argument(
-        '--split',
-        type=str,
-        default='sentence',
-        help="sentence, word"
-    )
-    parser.add_argument(
-        '--align',
-        type=str,
-        default='upperleft',
-        help="left, upperleft, center"
-    )
-    parser.add_argument(
-        '--font_size',
-        type=int,
-        default=24,
-        help="font size"
-    )
-    parser.add_argument(
-        '--size',
-        type=lambda s: [int(item) for item in s.split(',')],
-        default='128,128',
-        help="size of output image"
-    )
-    parser.add_argument(
-        '--mode',
-        type=str,
-        default='text',
-        help="'text' or 'font'"
-    )
+    parser.add_argument("--mode", type=str, default="text", help="'text' or 'font'")
 
     args = parser.parse_args(arg_input)
     return args
 
-def draw_text(text,font,outfile,size=[64,64],align='center',font_size=30.0):
+
+def draw_text(text, font, outfile, size=[64, 64], align="center", font_size=30.0):
     with Drawing() as draw:
-        with Image(width=size[0], height=size[1], background=Color('white')) as img:
+        with Image(width=size[0], height=size[1], background=Color("white")) as img:
             try:
                 draw.font = font
                 draw.font_size = font_size
                 draw.push()
-                draw.fill_color = Color('hsl(0%, 0%, 0%)')
+                draw.fill_color = Color("hsl(0%, 0%, 0%)")
                 try:
-                    mutable_message = word_wrap(img,
-                                                draw,
-                                                text,
-                                                size[0]-10,
-                                                size[1]-10)
+                    mutable_message = word_wrap(
+                        img, draw, text, size[0] - 10, size[1] - 10
+                    )
                 except RuntimeError as detail:
                     print(f"Error in word_wrap: {detail}")
                     return False
 
-                if align == 'center':
-                    draw.text(int(img.width/2)-font_size//4,int(img.height/2)+font_size//4, mutable_message)
-                elif align == 'left':
-                    draw.text(font_size//4,int(img.height/2)+font_size//4, mutable_message)
-                elif align == 'upperleft':
-                    draw.text(10,20, mutable_message)
+                if align == "center":
+                    draw.text(
+                        int(img.width / 2) - font_size // 4,
+                        int(img.height / 2) + font_size // 4,
+                        mutable_message,
+                    )
+                elif align == "left":
+                    draw.text(
+                        font_size // 4,
+                        int(img.height / 2) + font_size // 4,
+                        mutable_message,
+                    )
+                elif align == "upperleft":
+                    draw.text(10, 20, mutable_message)
                 else:
-                    draw.text(0,0, text)
+                    draw.text(0, 0, text)
                 draw.pop()
                 draw(img)
                 img.save(filename=outfile)
@@ -125,44 +104,56 @@ def draw_text(text,font,outfile,size=[64,64],align='center',font_size=30.0):
                 return False
     return False
 
+
 def split_on_sentence(data):
     from nltk.tokenize import sent_tokenize, word_tokenize
+
     tokenized = sent_tokenize(data)
     new_tokenized = []
     for sent in tokenized:
-            #print(sent)
-            done = False
-            while True:
-                if len(sent) < 5:
-                    break
-                new_tokenized.append(sent[:65])
-                sent = sent[65:]
+        while True:
+            if len(sent) < 5:
+                break
+            new_tokenized.append(sent[:65])
+            sent = sent[65:]
 
     return new_tokenized
 
-            
+
 def split_on_word(filepath):
     from nltk.tokenize import sent_tokenize, word_tokenize
+
     fp = open(filepath)
     data = fp.read()
     tokenized = word_tokenize(data)
-    #print(tokenized)
+    # print(tokenized)
     return tokenized
 
 
 def create_char_dataset(args):
-    char_options = '0123456789abcdefghijklmnopqrstwxyzABCDEFGHIJKLMNOPQRSTWXYZ'
+    char_options = "0123456789abcdefghijklmnopqrstwxyzABCDEFGHIJKLMNOPQRSTWXYZ"
     start_time = time.time()
     i = 0
     info = []
     for text in char_options:
-        for root, dirs, files in os.walk('/home/rs/media/game-assets/fonts/10000-fonts-combined'):
+        for root, dirs, files in os.walk(
+            "/home/rs/media/game-assets/fonts/10000-fonts-combined"
+        ):
             for (i, font_file) in enumerate(files):
-                font = os.path.join(root,font_file)
-                fontname = ''.join(c for c in os.path.splitext(font_file)[0] if c.isalnum())
+                font = os.path.join(root, font_file)
+                fontname = "".join(
+                    c for c in os.path.splitext(font_file)[0] if c.isalnum()
+                )
                 outfile = f"{fontname}_{text}_{i}.png"
-                outpath = os.path.join(args.out,outfile)
-                drawsuccess = draw_text(text,font,outpath,size=args.size,align=args.align,font_size=args.font_size)
+                outpath = os.path.join(args.out, outfile)
+                drawsuccess = draw_text(
+                    text,
+                    font,
+                    outpath,
+                    size=args.size,
+                    align=args.align,
+                    font_size=args.font_size,
+                )
                 if drawsuccess:
                     info.append(
                         {
@@ -183,19 +174,27 @@ def create_char_dataset(args):
         )
         f.write(jsondata)
 
-def create_text_dataset(args,data):
-    font = '/home/rs/.fonts/DroidSans.ttf'
+
+def create_text_dataset(args, data):
+    font = "/home/rs/.fonts/DroidSans.ttf"
     start_time = time.time()
     i = 0
     text_img_pairs = []
 
     for text in tqdm(data):
-        name = ''.join(c for c in text if c.isalnum())
-        name = ''.join([ch if ch != ' ' else '_' for ch in name])
-        outfile = name+'-'+str(i)+".png"
-        outpath = os.path.join(args.out,outfile)
+        name = "".join(c for c in text if c.isalnum())
+        name = "".join([ch if ch != " " else "_" for ch in name])
+        outfile = name + "-" + str(i) + ".png"
+        outpath = os.path.join(args.out, outfile)
         try:
-            drawsuccess = draw_text(text,font,outpath,size=args.size,align=args.align,font_size=args.font_size)
+            drawsuccess = draw_text(
+                text,
+                font,
+                outpath,
+                size=args.size,
+                align=args.align,
+                font_size=args.font_size,
+            )
             if drawsuccess:
                 text_img_pairs.append([text, outfile])
         except Exception as detail:
@@ -213,8 +212,9 @@ def create_text_dataset(args,data):
             }
         )
         f.write(jsondata)
-        #if i > 10:
-            #break
+        # if i > 10:
+        # break
+
 
 def word_wrap(image, ctx, text, roi_width, roi_height):
     """Break long text to multiple lines, and reduce point size
@@ -241,7 +241,7 @@ def word_wrap(image, ctx, text, roi_width, roi_height):
             columns = len(mutable_message)
             while columns > 1:
                 columns -= 1
-                mutable_message = '\n'.join(wrap(mutable_message, columns))
+                mutable_message = "\n".join(wrap(mutable_message, columns))
                 wrapped_width, _ = eval_metrics(mutable_message)
                 if wrapped_width <= roi_width:
                     break
@@ -252,8 +252,6 @@ def word_wrap(image, ctx, text, roi_width, roi_height):
     if iteration_attempts < 1:
         raise RuntimeError("Unable to calculate word_wrap for " + text)
     return mutable_message
-
-
 
 
 def main(args=None):
@@ -269,25 +267,25 @@ def main(args=None):
         for root, dirs, files in os.walk(args.dirpath):
             for f in files:
                 try:
-                    fp = open(os.path.join(root,f))
+                    fp = open(os.path.join(root, f))
                     data = fp.read()
                     data = data.replace("\r\n", " ")
-                    data = data.replace('\n',' ')
+                    data = data.replace("\n", " ")
                     data = data.replace("\r", " ")
                     print(data)
                 except Exception as detail:
                     print(detail)
                     continue
 
-                if args.split == 'word':
+                if args.split == "word":
                     data = split_on_word(data)
                 else:
                     data = split_on_sentence(data)
                 processed_data += data
-        create_text_dataset(args,processed_data)
+        create_text_dataset(args, processed_data)
     else:
         raise RuntimeError("invalid mode")
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
--- make_emoji_senses_dataset.py
+++ make_emoji_senses_dataset.py
@@ -19,26 +19,33 @@ from tqdm import tqdm
 
 app = typer.Typer()
 
+
 @app.command()
-def save_emoji(indexfile: str, outdir: str = "emoji-senses/", size: int = 256, maxexamples: int = 1e20, overwrite: bool = False):
+def save_emoji(
+    indexfile: str,
+    outdir: str = "emoji-senses/",
+    size: int = 256,
+    maxexamples: int = 1e20,
+    overwrite: bool = False,
+):
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     info = []
     with open(indexfile, "r") as f:
         data = json.loads(f.read())
-    for i,emoji in tqdm(enumerate(data), total=len(data)):
+    for i, emoji in tqdm(enumerate(data), total=len(data)):
         try:
             urlmatches, imgtaglist = to_png(emoji)
-            for i,(urlmatch,imgtag) in enumerate(zip(urlmatches,imgtaglist)):
+            for i, (urlmatch, imgtag) in enumerate(zip(urlmatches, imgtaglist)):
                 name = emoji["name"].replace(" ", "-")
                 name = name.replace(":", "-")
                 filename = f"{name}_{i}.png"
-                outfile= Path(outdir)/Path(filename)
+                outfile = Path(outdir) / Path(filename)
                 if outfile.is_file() and not overwrite:
                     # Already exists, don't waste time
                     info.append(
                         {
-                            "text": emoji['name'],
+                            "text": emoji["name"],
                             "filename": filename,
                             "providernum": i,
                             "url": str(urlmatch),
@@ -50,14 +57,14 @@ def save_emoji(indexfile: str, outdir: str = "emoji-senses/", size: int = 256, m
 
                 response = requests.get(urlmatch, stream=True)
                 if response.status_code == 200:
-                    with open(outfile, 'wb') as f:
+                    with open(outfile, "wb") as f:
                         img = Image.open(response.raw)
-                        img = img.resize((size, size), Image.LANCZOS)# add args
+                        img = img.resize((size, size), Image.LANCZOS)  # add args
                         img.save(f)
                         del response
                     info.append(
                         {
-                            "text": emoji['name'],
+                            "text": emoji["name"],
                             "filename": filename,
                             "providernum": i,
                             "url": str(urlmatch),
@@ -65,7 +72,7 @@ def save_emoji(indexfile: str, outdir: str = "emoji-senses/", size: int = 256, m
                             "imgtag": imgtag,
                         }
                     )
-                    for k,v in emoji["senses"].items():
+                    for k, v in emoji["senses"].items():
                         for codes in v:
                             for code, descriptions in codes.items():
                                 for description in descriptions:
@@ -90,7 +97,7 @@ def save_emoji(indexfile: str, outdir: str = "emoji-senses/", size: int = 256, m
     jsonfile = Path(outdir) / Path("index.json")
     with open(jsonfile, "w+") as f:
         print(f"JSON outfile: {jsonfile}")
-        #train, val = random_split(info, [len(info)-len(info)//20, len(info)//20])
+        # train, val = random_split(info, [len(info)-len(info)//20, len(info)//20])
         jsondata = json.dumps(
             {
                 "data": info,
@@ -98,37 +105,49 @@ def save_emoji(indexfile: str, outdir: str = "emoji-senses/", size: int = 256, m
         )
         f.write(jsondata)
 
+
 def to_png(emoji, version=0):
-    url = re.findall(r'http://.*/', emoji["definition"])[-1]
+    url = re.findall(r"http://.*/", emoji["definition"])[-1]
     data = requests.get(url).text
     html_search_string = r"<img.*alt=\".*{}.*height=\"120\"\>"  # Hacky height check to prevent smaller thumbnails being picked
-    matchlist = re.findall(html_search_string.format(emoji["name"]), data, flags=re.IGNORECASE)
-    urlmatches = [re.findall(r"\"https.*thumbs.*.png\"", match, flags=re.IGNORECASE) for match in matchlist]
+    matchlist = re.findall(
+        html_search_string.format(emoji["name"]), data, flags=re.IGNORECASE
+    )
+    urlmatches = [
+        re.findall(r"\"https.*thumbs.*.png\"", match, flags=re.IGNORECASE)
+        for match in matchlist
+    ]
     cleanmatches = []
     for sublist in urlmatches:
         for urlmatch in sublist:
             cleanmatches.append(urlmatch)
-            
-    cleanmatches = [urlmatch.strip('"') for urlmatch in cleanmatches]
-    #import ipdb; ipdb.set_trace()
-    return cleanmatches, matchlist
 
+    cleanmatches = [urlmatch.strip('"') for urlmatch in cleanmatches]
+    # import ipdb; ipdb.set_trace()
+    return cleanmatches, matchlist
 
 
 class EmojiConverter:
     def __init__(self):
-        self.data = requests.get("https://unicode.org/emoji/charts/full-emoji-list.html").text
-        self.skintones_data = requests.get("https://unicode.org/emoji/charts/full-emoji-modifiers.html").text
+        self.data = requests.get(
+            "https://unicode.org/emoji/charts/full-emoji-list.html"
+        ).text
+        self.skintones_data = requests.get(
+            "https://unicode.org/emoji/charts/full-emoji-modifiers.html"
+        ).text
 
     def to_base64_png(self, emoji, version=0):
         """For different versions, you can set version = 0 for ,"""
-        html_search_string = r"<img alt='{}' class='imga' src='data:image/png;base64,([^']+)'>"  #'
+        html_search_string = (
+            r"<img alt='{}' class='imga' src='data:image/png;base64,([^']+)'>"  #'
+        )
         try:
             matchlist = re.findall(html_search_string.format(emoji), self.data)
         except Exception:
-            matchlist = re.findall(html_search_string.format(emoji), self.skintones_data)
+            matchlist = re.findall(
+                html_search_string.format(emoji), self.skintones_data
+            )
         return matchlist[version]
-
 
 
 if __name__ == "__main__":
